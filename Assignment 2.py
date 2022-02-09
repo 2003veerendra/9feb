9ff920d22a3711b55083820861716421a05ed3b9
2003veerendra <99268975+2003veerendra@users.noreply.github.com>
2022-02-09 20:56:54 +0530

    Create Assignment 2.py

---
+++ Assignment 2.py
@@ -0,0 +1,11 @@
+a=int(input('Enter a number'))
+if(a%2==0):
+ print('A is even')
+else:
+    print('A is odd')
+x = int(input("Enter x value"))
+y = int(input("Enter y value"))
+if(x>y):
+    print('x is big')
+else:
+    print('y is big')
